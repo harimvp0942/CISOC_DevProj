124f2ead7634d5fd41f26f35064b041232dd6682
harimvp0942 <104444100+harimvp0942@users.noreply.github.com>
2022-04-26 11:43:03 -0400

    Add files via upload

---
+++ ClientApp/node_modules/Models/PageContent.cs
@@ -0,0 +1,11 @@
+﻿namespace CASIO_HariKrishna.Models
+{
+    public class PageContent
+    { 
+        public string FileName { get; set; }
+
+        public string ObjectContent { get; set; }
+
+        public int Id { get; set; }
+    }
+}
---
+++ ClientApp/node_modules/Models/UserDetailModel.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace CASIO_HariKrishna.Models
+{
+    public class UserDetailModel
+    {
+        public UserDetailModel()
+        {
+
+        }
+
+        [JsonProperty("name")]
+        public string UserName { get; set; }
+    }
+}
